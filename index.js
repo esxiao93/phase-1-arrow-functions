ebe693c6917789c3ea4c8348f5c9227f745a64af
Eric Xiao <es.xiao93@gmail.com>
2022-04-05 10:43:35 -0400

    commit tree 64eec22d3d805ceed418f1825d54ee1e49bb5afd

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = x => 2000 / 100;
+divide;
+
+const square = x => x ** x
+square;
+
+const add = (num1, num2) => {
+console.log(`add ${num1}`);
+console.log(`add ${num2}`);
+return num1 + num2;
+}
